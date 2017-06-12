d2704cf342b643bf72d6af8adb7d4f7ebd51a0a0
20161103403 <1228744270@qq.com>
2017-06-12 11:47:00 +0800

    测试
    排序

--- paixu/main.c
+++ paixu/main.c
@@ -1,15 +1,23 @@
-//
-//  main.c
-//  paixu
-//
-//  Created by 20161104603 on 2017/6/12.
-//  Copyright © 2017年 20161104603. All rights reserved.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main()
+{
+    int n[11],i,j,t,k;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=0;j<9-i;j++){
+            if(n[j]>n[j+1])
+            {
+            t=n[j];
+            n[j]=n[j+1];
+            n[j+1]=t;
+        }
+        }
+    }
+    for(k=0;k<10;k++) printf("%d ",n[k]);
+    printf("\n");
     return 0;
 }
